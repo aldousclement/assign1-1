67f641beb4ccb90622b4c9db87896b1ba9171bcb
aldousclement <165456036+aldousclement@users.noreply.github.com>
2024-03-30 14:12:59 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string username, password;
+        int attempts = 3;
+
+        do
+        {
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+
+            if (username == "Admin" && password == "Admin1234!")
+            {
+                Console.WriteLine("Login succesful!");
+                break; 
+            }
+            else
+            {
+                Console.WriteLine("Incorrect credentials. Attempts left: " + --attempts);
+            }
+        } while (attempts > 0);
+
+        if (attempts  == 0)
+        {
+            Console.WriteLine("Maximum attempts reached. Login failed");
+        }
+    }
+}
